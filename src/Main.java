d3fa4b8089b381564a059343aa4bd2a28fca4956
Tatyana58 <tatbir@rambler.ru>
2022-12-02 22:55:12 +0300

    Beginning Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Дамашняя работа: Массивы 2");
+        int [] arr=new int[5];
+        fillRandom(arr,0);
+        System.out.println(Arrays.toString(arr));
+        fillRandom(arr,0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void fillRandom(int[] arr, int n) { //заполнение массива случайными числами
+        Random r =new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]= r.nextInt(100)+100;  //диапазон значений от 100_000 до 200_000
+        }
     }
 }
